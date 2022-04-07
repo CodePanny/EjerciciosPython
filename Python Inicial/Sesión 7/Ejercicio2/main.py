fa80162d46ce42a068cd7537fe3a0184684bf5de
RedPanda <93345358+CodePanny@users.noreply.github.com>
2022-04-07 20:56:28 +0200

    Update main.py
    code updated

--- Python Inicial/Sesión 7/Ejercicio2/main.py
+++ Python Inicial/Sesión 7/Ejercicio2/main.py
@@ -1,11 +1,22 @@
 import time
 
+horaActual = time.strftime('%H:%M:%S')
+
 H = time.strftime('%H')
 M = time.strftime('%M')
 S = time.strftime('%S')
+
+cH = 18 - int(H)
+cM = 59 - int(M)
+cS = 59 - int(S)
+
+trabaja = f"Aún toca trabajar, quedan {cH} horas, {cM} minutos y {cS} segundos para terminar"
+
+
+print("Son las", horaActual)
 
 if int(H) >= 19:
     print("Go home bro")
 
 else:
-    print("Aún toca trabajar, quedan {} horas, {} minutos y {} segundos para terminar".format(18 - int(H), 59 - int(M), 59 - int(S)))
+    print(trabaja)
